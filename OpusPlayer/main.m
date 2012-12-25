e8ee949e8a5563fe9f4bddf50b2f2d157b9ec2ff
Chris van Engelen <cvengelen@imaccve.lan>
2012-12-25 23:27:49 +0100

    Add magic to avoid Mac to go to sleep when OpusPlayer is active

--- OpusPlayer/main.m
+++ OpusPlayer/main.m
@@ -8,7 +8,34 @@
 
 #import <Cocoa/Cocoa.h>
 
+// Technical Q&A QA1340: Preventing sleep using I/O Kit: 
+#import <IOKit/pwr_mgt/IOPMLib.h>
+
 int main(int argc, char *argv[])
 {
-    return NSApplicationMain(argc, (const char **)argv);
+    // reasonForActivity is used by the system whenever it needs to tell
+    // the user why the system is not sleeping (limited to 128 chararcers).
+    CFStringRef reasonForActivity = CFSTR( "OpusPlayer is active");
+    
+    IOPMAssertionID assertionID;
+    IOReturn assertionCreateWithNameReturn = IOPMAssertionCreateWithName( kIOPMAssertionTypeNoDisplaySleep,
+                                                                          kIOPMAssertionLevelOn, reasonForActivity, &assertionID );
+    if ( assertionCreateWithNameReturn != kIOReturnSuccess )
+    {
+        NSLog( @"IOPMAssertionCreateWithName failed with error code %d", assertionCreateWithNameReturn );
+        return 1;
+    }
+
+    int applicationMainReturn = NSApplicationMain(argc, (const char **)argv);
+    
+    // Release the assertion, to enable the system to be able to sleep again
+    assertionCreateWithNameReturn = IOPMAssertionRelease( assertionID );
+    if ( assertionCreateWithNameReturn != kIOReturnSuccess )
+    {
+        NSLog( @"IOPMAssertionRelease failed with error code %d", assertionCreateWithNameReturn );
+        return 1;
+    }
+    
+    // Return the value from NSApplicationMain
+    return applicationMainReturn;
 }
